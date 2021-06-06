270ad04fc69d63b5388ffc988b31d091b033f308
Zachary Flanagan <zacharyf24147@gmail.com>
2021-06-06 05:12:59 -0400

    Initial solution to exercise 1

--- src/main/java/ex01/App.java
+++ src/main/java/ex01/App.java
@@ -1,4 +1,14 @@
 package ex01;
 
+import java.util.Scanner;
+
 public class App {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("What is your name? ");
+        String name = in.nextLine();
+
+        String outputString = "Hello, " + name + ", nice to meet you!";
+        System.out.println(outputString);
+    }
 }
